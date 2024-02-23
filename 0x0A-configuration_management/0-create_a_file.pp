edbc7447a997c70ff6e9057043be785f4e795a47
hardeyphemmy <adegboyefemi104@gmail.com>
2024-02-23 19:57:33 +0000

    create puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+file {'/tmp/school':
+  ensure  => 'file',      # Ensure is a file
+  mode    => '0744',      # File permission
+  owner   => 'www-data',   # File owner
+  group   => 'www-data',   # File group
+  content => "I love puppet\n", # Content
+}
